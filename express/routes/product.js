631efd72e97644de07bafee06cf50403a426a2bf
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-03 00:01:18 +0100

    Updated product routes

--- express/routes/product.js
+++ express/routes/product.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const productController = require('../controllers/productController');
-
-
-
-
 const router = express.Router();
 
 router.get('/', productController.getAllProducts);
 router.post('/', productController.createProduct);
-router.put('/:id', productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
+router.put('/:ref', productController.updateProduct);
+router.delete('/:ref', productController.deleteProduct);
 
 module.exports = router;
