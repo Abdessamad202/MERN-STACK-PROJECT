112629ba7dccff623b353ba5d0419fc1c0817268
abdessamad <[200~git config --global user.email kechaf.abdo.ka.2018@gmail.com>
2025-01-04 00:54:56 +0100

    Updated user controller~

--- express/controllers/userController.js
+++ express/controllers/userController.js
@@ -40,7 +40,7 @@ const login = async (req, res) => {
   const validPass = await bcrypt.compare(req.body.password, user.password);
   if (!validPass) return res.status(400).json({ message: 'Invalid password' });
 
-  const token = jwt.sign({ _id: user._id }, process.env.MY_SECRET_KEY);
+  const token = jwt.sign({ _id: user._id }, "your_secret_key");
   res.header('auth-token', token).json({ token,userId: user._id });
 };
 
