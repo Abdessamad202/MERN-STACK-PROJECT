5cf69e7e4553aa49e6563aeaf85c585ce79cfbe7
abdessamad <[200~git config --global user.email kechaf.abdo.ka.2018@gmail.com>
2025-01-04 00:55:09 +0100

    Updated index.js

--- express/index.js
+++ express/index.js
@@ -3,11 +3,19 @@ const mongoose = require('mongoose');
 const productRoutes = require('./routes/product');
 const bookRoutes = require('./routes/book');
 const userRoutes = require('./routes/user');
+const cors = require('cors');
 const reservationRoutes = require('./routes/reservation'); // Add this line
-
 const app = express();
 
+// More specific CORS configuration (recommended for production):
+const corsOptions = {
+  origin: 'http://127.0.0.1:5500', // Only allow requests from this origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+  credentials: true, // Allow sending cookies
+  optionsSuccessStatus: 204 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 // Middleware
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
